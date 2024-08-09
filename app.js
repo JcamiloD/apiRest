ad5318c611ca8bd713eb7353423e3a326a7713f1
JcamiloD <jcamiloarbelaezdiaz@gmail.com>
2024-08-09 14:52:53 -0500

    ola

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: 'localhost',  
     user: 'root',       
-    password: '1234',       
+    password: '',       
     database: 'yamix' 
 });
 
 
 db.connect((err) => {
     if (err) {
-        console.error('Error al conectar a la base de datos MySQL:', err);
+        console.error(' si no trabajan los mato Error al conectar a la base de datos MySQL:', err);
     } else {
         console.log('Conectado a la base de datos MySQL.');
     }
