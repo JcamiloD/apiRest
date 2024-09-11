110a85b98808b9963318e4c8e0821793e7522486
JuanBeta12 <juaebetancur1@misena.edu.co>
2024-09-11 00:19:46 -0500

    calendario juanes

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const auth = require('./routes/auth.routes') 
-const usuario =require('./routes/crud_estudiantes.routes')
-const clase =require('./routes/crud_clases.routes')
-const roles =require('./routes/roles.routes')
+const auth = require('./routes/auth.routes');
+const usuario = require('./routes/crud_estudiantes.routes');
+const clase = require('./routes/crud_clases.routes');
+const roles = require('./routes/roles.routes');
+const eventos = require('./routes/eventos.routes'); // Asegúrate de que el archivo es correcto
 
 require('dotenv').config();
-
-
-
 
 const app = express();
 app.use(express.json());
@@ -16,20 +14,17 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
+const cors = require('cors');
+app.use(cors());
 
-app.use('/api', auth, usuario, clase, roles)   
+app.use('/api', auth, usuario, clase, roles, eventos); // Incluye las rutas de eventos
 
 app.use((req, res, next) => {
     console.log(`Solicitud ${req.method} recibida en ${req.url}`);
     next();
 });
 
-const cors = require('cors');
-app.use(cors());
-
-
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
-
