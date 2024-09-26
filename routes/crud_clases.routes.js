bc459f0adef91f044bab7434fd2d350fdec9feb0
Mathiws112 <mathiws112@gmail.com>
2024-09-25 20:38:12 -0500

    Ya

--- routes/crud_clases.routes.js
+++ routes/crud_clases.routes.js
@@ -5,8 +5,6 @@ const clase = require('../controller/crud_clases');
 
 const router = express.Router();
 
-router.post('/agregar_clase',clase.agregarClase)
 router.get('/traer_clases',clase.traer)
-router.delete('/eliminar/:id', clase.eliminar)
 
 module.exports = router;
